de6a57c76bf21aeb199ed14c8122aba8ceee8374
CrpShepard <Valeraiq@mail.ru>
2023-09-22 06:08:19 +0900

    task_5

--- spring-app1/src/main/java/ru/crpshepard/springcourse/TestSpring.java
+++ spring-app1/src/main/java/ru/crpshepard/springcourse/TestSpring.java
@@ -12,11 +12,23 @@ public class TestSpring {
 
         //MusicPlayer musicPlayer = new MusicPlayer(music);
 
-        MusicPlayer musicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
-        musicPlayer.playMusic();
+        MusicPlayer firstMusicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
+        MusicPlayer secondMusicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
 
-        System.out.println(musicPlayer.getName());
-        System.out.println(musicPlayer.getVolume());
+        boolean comparison = firstMusicPlayer == secondMusicPlayer;
+
+        System.out.println(comparison);
+
+        System.out.println(firstMusicPlayer);
+        System.out.println(secondMusicPlayer);
+
+        firstMusicPlayer.setVolume(10);
+
+        System.out.println(firstMusicPlayer.getVolume());
+        System.out.println(secondMusicPlayer.getVolume());
+
+        //System.out.println(musicPlayer.getName());
+        //System.out.println(musicPlayer.getVolume());
 
 
         context.close();
