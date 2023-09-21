d8a0efb8d2c66aae68229030db1d6f7deed27a0d
CrpShepard <Valeraiq@mail.ru>
2023-09-22 05:50:18 +0900

    task_4

--- spring-app1/src/main/java/ru/crpshepard/springcourse/TestSpring.java
+++ spring-app1/src/main/java/ru/crpshepard/springcourse/TestSpring.java
@@ -15,6 +15,10 @@ public class TestSpring {
         MusicPlayer musicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
         musicPlayer.playMusic();
 
+        System.out.println(musicPlayer.getName());
+        System.out.println(musicPlayer.getVolume());
+
+
         context.close();
     }
 }
