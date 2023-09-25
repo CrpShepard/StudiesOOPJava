e1a42a931ccce8d34f9e10d743d892503e39f00d
CrpShepard <54824792+CrpShepard@users.noreply.github.com>
2023-09-25 09:53:09 +0900

    task_7

--- spring-app1/src/main/java/ru/crpshepard/springcourse/RockMusic.java
+++ spring-app1/src/main/java/ru/crpshepard/springcourse/RockMusic.java
@@ -1,5 +1,8 @@
 package ru.crpshepard.springcourse;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class RockMusic implements Music{
     @Override
     public String getSong() {
--- spring-app1/src/main/java/ru/crpshepard/springcourse/TestSpring.java
+++ spring-app1/src/main/java/ru/crpshepard/springcourse/TestSpring.java
@@ -8,11 +8,17 @@ public class TestSpring {
             "applicationContext.xml"
         );
 
-        Music music = context.getBean("musicBean", Music.class);
+        Music music = context.getBean("classicalMusic", Music.class);
 
         MusicPlayer musicPlayer = new MusicPlayer(music);
 
         musicPlayer.playMusic();
+
+        Music music2 = context.getBean("rockMusic", Music.class);
+
+        MusicPlayer musicPlayer2 = new MusicPlayer(music2);
+
+        musicPlayer2.playMusic();
 
         context.close();
     }
