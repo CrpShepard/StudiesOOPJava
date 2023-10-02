fdff8dc49081d39bd95c70e21b4fa125f2691bb5
CrpShepard <54824792+CrpShepard@users.noreply.github.com>
2023-10-03 00:14:53 +0900

    task_10

--- spring-app1/src/main/java/ru/crpshepard/springcourse/TestSpring.java
+++ spring-app1/src/main/java/ru/crpshepard/springcourse/TestSpring.java
@@ -24,8 +24,18 @@ public class TestSpring {
 
         // musicPlayer.playMusic();
 
-        Computer computer = context.getBean("computer", Computer.class);
-        System.out.println(computer.toString());
+        // Computer computer = context.getBean("computer", Computer.class);
+        // System.out.println(computer.toString());
+
+        MusicPlayer musicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
+
+        System.out.println(musicPlayer.getName());
+        System.out.println(musicPlayer.getVolume());
+
+        ClassicalMusic classicalMusic1 = context.getBean("classicalMusic", ClassicalMusic.class);
+        ClassicalMusic classicalMusic2 = context.getBean("classicalMusic", ClassicalMusic.class);
+
+        System.out.println(classicalMusic1 == classicalMusic2);
 
         context.close();
     }
