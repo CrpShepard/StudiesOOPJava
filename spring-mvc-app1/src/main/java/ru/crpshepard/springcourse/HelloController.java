4ea43ce606e944f144d70cce6540585b1b4235ff
CrpShepard <Valeraiq@mail.ru>
2023-10-03 23:10:37 +0900

    Решена проблема с нахождением своей ссылки
    Spring 6 -> 5 переход версии
    Возможно шаблонизатор thymeleaf имеет свои проблемы

--- spring-mvc-app1/src/main/java/ru/crpshepard/springcourse/HelloController.java
+++ spring-mvc-app1/src/main/java/ru/crpshepard/springcourse/HelloController.java
@@ -2,13 +2,11 @@ package ru.crpshepard.springcourse;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
     
-    @GetMapping("hello-world")
-    @ResponseBody
+    @GetMapping("/hello")
     public String sayHello() {
         return "hello_world";
     }
