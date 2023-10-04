d7bccdcd606d255abb8222f3546212459d9694aa
CrpShepard <Valeraiq@mail.ru>
2023-10-05 02:09:11 +0900

    Проблемы с сервером apache tomcat

--- spring-mvc-app1/src/main/java/ru/crpshepard/springcourse/controllers/FirstController.java
+++ spring-mvc-app1/src/main/java/ru/crpshepard/springcourse/controllers/FirstController.java
@@ -1,5 +1,7 @@
 package ru.crpshepard.springcourse.controllers;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,7 +10,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/first")
 public class FirstController {
     @GetMapping("/hello")
-    public String helloPage() {
+    public String helloPage(HttpServletRequest request) {
+        String name = request.getParameter("name");
+        String surname = request.getParameter("surname");
+
+        System.out.println("Hello, " + name + " " + surname + " !");
+
         return "first/hello";
     }
 
