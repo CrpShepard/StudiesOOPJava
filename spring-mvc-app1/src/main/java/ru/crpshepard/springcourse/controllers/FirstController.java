135ed2fbef500785a46bef1996d364ab92e31cb4
CrpShepard <54824792+CrpShepard@users.noreply.github.com>
2023-10-09 18:15:48 +0900

    task_16_mvc не закончил вроде

--- spring-mvc-app1/src/main/java/ru/crpshepard/springcourse/controllers/FirstController.java
+++ spring-mvc-app1/src/main/java/ru/crpshepard/springcourse/controllers/FirstController.java
@@ -3,6 +3,7 @@ package ru.crpshepard.springcourse.controllers;
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -12,9 +13,12 @@ import org.springframework.web.bind.annotation.RequestParam;
 public class FirstController {
     @GetMapping("/hello")
     public String helloPage(@RequestParam(value = "name", required = false) String name,
-                            @RequestParam(value = "surname", required = false) String surname) {
+                            @RequestParam(value = "surname", required = false) String surname,
+                            Model model) {
 
-        System.out.println("Hello, " + name + " " + surname + " !");
+        //System.out.println("Hello, " + name + " " + surname + " !");
+
+        model.addAttribute("message", "Hello, " + name + " " + surname + " !");
 
         return "first/hello";
     }
@@ -22,5 +26,19 @@ public class FirstController {
     @GetMapping("/goodbye")
     public String goodbyePage() {
         return "first/goodbye";
+    }
+
+    @GetMapping("/calculator")
+    public String calculator(@RequestParam("a") int a,
+                            @RequestParam("b") int b,
+                            @RequestParam("action") String action,
+                            Model model) {
+        double result;
+
+        switch(action) {
+            case 
+        }
+
+        return "";
     }
 }
